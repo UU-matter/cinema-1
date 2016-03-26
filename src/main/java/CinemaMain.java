3967062e953576a3fa512146c34d1d91c17cdb8b
Mad.Pot <badguns66@gmail.com>
2016-03-26 13:38:26 +0400

    VER0.3

--- src/main/java/CinemaMain.java
+++
@@ -1,8 +0,0 @@
-import java.sql.SQLException;
-
-public class CinemaMain {
-    public static void main(String[] args) throws SQLException {
-        CreateDB db = new CreateDB();
-        db.createTables();
-    }
-}
